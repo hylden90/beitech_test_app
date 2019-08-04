0c8d242048bd74ea2b8a3d750045e5d6f3492141
Dihego <dfbautistat@bitbucket.org>
2019-08-03 23:16:38 -0500

    Correct typo: renamed attribute from customer_id to customer.

--- orders/models/order.py
+++ orders/models/order.py
@@ -5,5 +5,5 @@ from .product import Product
 class Order(models.Model):
     total_price = models.IntegerField(default=0)
     creation_date = models.DateTimeField()
-    customer_id = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
+    customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
     products = models.ManyToManyField(Product)
