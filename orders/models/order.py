82b7151dbe0babe8387fb2dc5e0e2e1facdc43d0
Dihego <dfbautistat@bitbucket.org>
2019-08-03 23:48:42 -0500

    Set order model default creation time to the moment it is instantiated.

--- orders/models/order.py
+++ orders/models/order.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils import timezone
 from .customer import Customer
 from .product import Product
 
 class Order(models.Model):
     total_price = models.IntegerField(default=0)
-    creation_date = models.DateTimeField()
+    creation_date = models.DateTimeField(default=timezone.now)
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
     products = models.ManyToManyField(Product)
