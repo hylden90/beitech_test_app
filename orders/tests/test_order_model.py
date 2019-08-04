f3f9dd2e36fa819323b21f8615828ae484efc4ae
Dihego <dfbautistat@bitbucket.org>
2019-08-03 23:53:29 -0500

    Added test to check if an error occurs when an order is created with products not available to the customer.

---
+++ orders/tests/test_order_model.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+
+from orders.models import Order, Customer, Product
+
+class OrderModelTests(TestCase):
+
+    def test_when_try_create_order_with_no_allowed_products_for_customer_raise_value_error(self):
+        
+        new_customer = Customer(name='Mike', email='mike@mi.ke')
+        new_customer.save()
+        milk = Product(name='Milk', price=10)
+        milk.save()
+        egg = Product(name='Egg', price=3)
+        egg.save()
+        cheese = Product(name='Cheese', price=8)
+        cheese.save()
+        
+        #Mike can buy milk and cheese
+        new_customer.allowed_products.add(milk)
+        new_customer.allowed_products.add(cheese)
+
+        #Mike wants to buy milk and two eggs!
+        invalid_order = Order()
+        invalid_order.customer = new_customer
+        invalid_order.save()
+        invalid_order.products.add(milk)
+        invalid_order.products.add(egg)
+        invalid_order.products.add(egg)
+
+        self.assertRaises(ValueError, invalid_order.save)
